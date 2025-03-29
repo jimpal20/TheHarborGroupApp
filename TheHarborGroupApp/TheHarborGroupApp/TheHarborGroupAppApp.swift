bc62c19565814e00a5db1391f60fb159693535bc
jimpal20 <jim@harborgroupohio.com>
2025-03-29 14:57:27 -0400

    Initial commit

---
+++ TheHarborGroupApp/TheHarborGroupApp/TheHarborGroupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheHarborGroupAppApp.swift
+//  TheHarborGroupApp
+//
+//  Created by Jim Powell on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TheHarborGroupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheHarborGroupApp/TheHarborGroupAppTests/TheHarborGroupAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheHarborGroupAppTests.swift
+//  TheHarborGroupAppTests
+//
+//  Created by Jim Powell on 3/29/25.
+//
+
+import Testing
+@testable import TheHarborGroupApp
+
+struct TheHarborGroupAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
